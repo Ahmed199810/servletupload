9a0d53d71a00ce2f9371dbfc7385b6a911d23ccd
Ahmed Mostafa <ahmed.a199810@gmail.com>
2022-03-06 12:16:31 +0200

    3rd

--- src/main/java/com/ahmedstoreapptest/test/Test.java
+++ src/main/java/com/ahmedstoreapptest/test/Test.java
@@ -1,16 +1,15 @@
 package com.ahmedstoreapptest.test;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 
 import java.io.IOException;
-import javax.*;
 
-/**
- * Servlet implementation class Test
- */
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
