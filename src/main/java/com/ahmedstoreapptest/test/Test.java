f381827bdce35c7828f6e1123d66b0ee539e93e6
Ahmed199810 <41450463+Ahmed199810@users.noreply.github.com>
2022-03-06 11:51:37 +0200

    Update Test.java

--- src/main/java/com/ahmedstoreapptest/test/Test.java
+++ src/main/java/com/ahmedstoreapptest/test/Test.java
@@ -1,12 +1,11 @@
 package com.ahmedstoreapptest.test;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import javax.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class Test
